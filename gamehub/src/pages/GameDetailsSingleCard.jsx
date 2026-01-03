5b93acf3a0548c00df8fec1d1857cbbd1b4e9c0f
Md. Sajid Ahmed <sajidr.u110011@gmail.com>
2026-01-03 11:05:18 +0600

    Game Detials Done

--- gamehub/src/pages/GameDetailsSingleCard.jsx
+++ gamehub/src/pages/GameDetailsSingleCard.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { Link, useLocation, useNavigate } from "react-router";
+import { IoMdArrowRoundBack } from "react-icons/io";
+import { IoStar } from "react-icons/io5";
+import { TbCategoryPlus } from "react-icons/tb";
+import { Link, NavLink, useLocation, useNavigate } from "react-router";
 
 const GameDetailsSingleCard = ({ games }) => {
   const navigate = useNavigate();
@@ -7,19 +10,42 @@ const GameDetailsSingleCard = ({ games }) => {
   const from = location.state?.from || "/";
   return (
     <div className="space-y-5">
-      <button className="btn btn-outline btn-sm" onClick={() => navigate(from)}>
-        Back
+      <button
+        className="btn btn-outline btn-primary"
+        onClick={() => navigate(from)}
+      >
+        <IoMdArrowRoundBack /> Back
       </button>
-      <img
-        className="w-full h-[350px] object-cover"
-        src={games.coverPhoto}
-        alt=""
-      />
-      <h2 className="text-2xl">{games.title}</h2>
-      {/* <p>{news.details}</p> */}
-      {/* <Link className="btn btn-secondary" to={`/category/${news.category_id}`}>
-        Back to category
-      </Link> */}
+
+      <div className="border p-5 rounded-2xl ">
+        <div className="hero bg-base-200">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <img
+              src={games.coverPhoto}
+              className="max-w-sm rounded-lg shadow-2xl"
+            />
+            <div className="flex flex-col">
+              <h1 className="text-5xl font-bold">{games.title}</h1>
+              <p className="py-6">{games.description}</p>
+              <div className="flex gap-3">
+                <div className="badge badge-outline badge-primary">
+                  <TbCategoryPlus /> {games.category}
+                </div>
+                <div className="badge badge-outline badge-primary">
+                  <IoStar /> {games.ratings}
+                </div>
+              </div>
+              <p className="mt-3">
+                Developed by:
+                <span className="font-bold"> {games.developer}</span>
+              </p>
+              <NavLink className="mt-3" target="_blank" to={games.downloadLink}>
+                <button className="btn btn-primary">Download Link</button>
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
